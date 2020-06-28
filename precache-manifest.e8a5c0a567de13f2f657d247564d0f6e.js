eb8fdaaaad22b5a0f9b79b542da48e11cae78887
SuperAiden <qpqp10212@gmail.com>
2020-06-28 21:25:31 +1000

    Updates

---
+++ precache-manifest.e8a5c0a567de13f2f657d247564d0f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867f63929c7e6a7c3acd5191b6bb1538",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58b5ee1f812a5fdf1a66",
+    "url": "/monsters-rolodex/static/css/main.91eaa33d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b5ee1f812a5fdf1a66",
+    "url": "/monsters-rolodex/static/js/main.5ad47ae0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
